825e919c5d48bf789e6588ed135939e3aec37b46
yogirpatel <yogirpatel@gmail.com>
2018-02-01 17:40:02 +0530

    update masketspider
    updated with normalise-space

--- market_beta/spiders/MarketBetaSpider.py
+++ market_beta/spiders/MarketBetaSpider.py
@@ -8,6 +8,10 @@ try:
     from urllib.parse import urlparse
 except ImportError:
      from urlparse import urlparse
+import re
+from scrapy.contrib.loader.processor import Compose, MapCompose
+from w3lib.html import replace_escape_chars, remove_tags
+
 
 class MarketBetaSpider(CrawlSpider):
    # print ("MBS2 Start")
@@ -22,37 +26,22 @@ class MarketBetaSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-
         hxs = Selector(response)
         item = MarketBetaItem()
-
-        item['url'] = response.request.url
-        
-        item['title'] = hxs.xpath('//*[@id="maincontent"]/div[2]/div/div[3]/div[2]/h1/span/text()').extract()
-	    
-        item['subTitle'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[2]/p/text()').extract()
-	    
-        item['productInfoMediaDetails'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[3]/text()').extract()
-	
-        item['description'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/div[1]/p[1]/span/text()').extract() + hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/text()').extract() + hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/div[1]/p[1]/text()').extract() + hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/div[1]/p/text()').extract()
-	
-        item['relatedProduct1'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[5]/div/div[2]/div/ol/li[1]/div/div/strong/a/text()').extract()
-	
-        item['relatedProduct2'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[5]/div/div[2]/div/ol/li[2]/div/div/strong/a/text()').extract()
-	
-        item['onDemand'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[4]/div[1]/div[1]/div[2]/text()').extract() 
-    
-        item['rent'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[4]/div[1]/div[2]/div[2]/form/div[1]/div/div/div/div/div[1]/label/text()').extract()
-	
-        item['price'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[4]/div[1]/div[2]/div[2]/form/div[1]/div/div/div/div/div[2]/label/text()').extract() 
-	
-        #NOTE:  ALSO SHOWING AS //*[@id="options-1360-list"]/div[1]/label
-        item['director'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/div[2]/text()[1]').extract()
-	
-        item['publisher'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/div[2]/text()[2]').extract()
-	    
+        #item.default_output_processor = MapCompose(lambda v: v.strip(), replace_escape_chars)		
+        item['url'] = response.request.url     			
+        item['title'] = hxs.xpath('//*[@id="maincontent"]/div[2]/div/div[3]/div[2]/h1/span/text()[normalize-space()]').extract()		
+        item['subTitle'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[2]/p/text()[normalize-space()]').extract()	    
+        item['productInfoMediaDetails'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[3]/text()[normalize-space()]').extract()
+        item['description'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/div[1]/p[1]/span/text()[normalize-space()]').extract() + hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/text()[normalize-space()]').extract() + hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/div[1]/p[1]/text()[normalize-space()]').extract() + hxs.xpath('.//*[@id="product__description"]/div[1]/p/text()[normalize-space()]').extract()
+        item['relatedProduct1'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[5]/div/div[2]/div/ol/li[1]/div/div/strong/a/text()[normalize-space()]').extract()
+        item['relatedProduct2'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[5]/div/div[2]/div/ol/li[2]/div/div/strong/a/text()[normalize-space()]').extract()
+        item['onDemand'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[4]/div[1]/div[1]/div[2]/text()[normalize-space()]').extract() 
+        item['rent'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[4]/div[1]/div[2]/div[2]/form/div[1]/div/div/div/div/div[1]/label/text()[normalize-space()]').extract()
+        item['price'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[4]/div[1]/div[2]/div[2]/form/div[1]/div/div/div/div/div[2]/label/text()[normalize-space()]').extract() 
+	    #NOTE:  ALSO SHOWING AS //*[@id="options-1360-list"]/div[1]/label
+        item['director'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/div[2]/text()[1][normalize-space()]').extract()   item['publisher'] = hxs.xpath('//html/body/main/div/main/div[2]/div/div[3]/div[4]/div[2]/text()[2][normalize-space()]').extract()  	
         yield item
-
         # Now Recurse
         page = response.url
         domain = urlparse(page).netloc
@@ -61,10 +50,10 @@ class MarketBetaSpider(CrawlSpider):
             if link.url == 'https://market.beta.formed.org/shop-products/watch.html':
                         yield {'link':link,'domain': domain}
                         print ("*************** ")					
-			if link.url == 'https://market.beta.formed.org/shop-products/listen.html':
+            elif link.url == 'https://market.beta.formed.org/shop-products/listen.html':
 						yield {'link':link,'domain': domain}
 						print ("*************** ")				
-			if link.url == 'https://market.beta.formed.org/shop-products/formed-subscriptions.html':
+            elif link.url == 'https://market.beta.formed.org/shop-products/formed-subscriptions.html':
 						yield {'link':link,'domain': domain}
 						print ("*************** ")
 						#print('MBS2- following %s from %s', link, domain)
